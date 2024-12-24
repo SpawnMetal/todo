76f040381d64597cf51d2a3716bec1a0b1fe4a98
SpawnMetal <spawnmetal666@gmail.com>
2024-12-25 01:13:34 +0700

    Updated 2024.12.25

--- client/components/main/main.tsx
+++ client/components/main/main.tsx
@@ -6,6 +6,6 @@ import {Mode} from '@components'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <Mode />,
+    <Mode />
   </React.StrictMode>,
 )
