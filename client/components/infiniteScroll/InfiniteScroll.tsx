d1e9510b88cc25eb46ff70978a04b968d53ec5e0
SpawnMetal <spawnmetal666@gmail.com>
2024-12-25 00:16:46 +0700

    Updated 2024.12.25

--- client/components/infiniteScroll/InfiniteScroll.tsx
+++ client/components/infiniteScroll/InfiniteScroll.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useMemo, memo} from 'react'
+import React, {useState, useEffect, useMemo, memo, useRef} from 'react'
 import {Progress} from '@components'
 import {Box} from '@mui/material'
 import * as style from './style'
@@ -9,18 +9,47 @@ export const InfiniteScroll = ({data, renderItem}) => {
   const [endIndex, setEndIndex] = useState(stepCount)
   const [page, setPage] = useState(1)
   const [loading, setLoading] = useState(false)
+  const isMaxLoaded = useRef(false)
+  const dataRef = useRef(true) // При первом рендере действия не требуются
+
+  // Обработка при добавлении новых элементов
+  useEffect(() => {
+    if (dataRef.current) dataRef.current = false
+    else {
+      if (loading || page * stepCount + stepCount <= data.length) return // В процессе загрузки действия ниже не требуются
+
+      // Если все элементы влазят
+      setEndIndex(data.length)
+      setPage(Math.ceil(endIndex / stepCount))
+    }
+  }, [data])
 
   useEffect(() => {
     const handleScroll = () => {
-      if (window.innerHeight + document.documentElement.scrollTop !== document.documentElement.offsetHeight) return
+      if (window.innerHeight + Math.ceil(document.documentElement.scrollTop) !== document.documentElement.offsetHeight) return
+      // Если не передать endIndex в зависимости, то endIndex будет всегда равна изначальному значению
+      if (endIndex >= data.length) return
+
       setLoading(true)
     }
 
     window.addEventListener('scroll', handleScroll)
 
     return () => window.removeEventListener('scroll', handleScroll)
-  }, [])
+  }, [endIndex])
 
+  // Загрузка следующих элементов, если все элементы влезли
+  useEffect(() => {
+    if (loading || isMaxLoaded.current) return
+    if (document.documentElement.offsetHeight <= window.innerHeight + Math.ceil(document.documentElement.scrollTop) && dataMemo.length < data.length) {
+      setLoading(true)
+      return
+    }
+
+    isMaxLoaded.current = true
+  }, [loading])
+
+  // Эмуляция загрузки элементов
   useEffect(() => {
     if (!loading) return
     if (page * stepCount >= data.length) return setLoading(false)
@@ -33,16 +62,8 @@ export const InfiniteScroll = ({data, renderItem}) => {
   }, [loading])
 
   const dataMemo = useMemo(() => {
-    const result = []
-
-    for (let i = 0; i < endIndex; i++) {
-      if (data[i] === undefined) break
-
-      result.push(renderItem(data[i]))
-    }
-
-    return result
-  }, [data, page])
+    return data.slice(0, endIndex).map(renderItem)
+  }, [data, endIndex])
 
   return (
     <>
